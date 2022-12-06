51fec2456b600228800009489c7e3ed60b584661
Nikolay <labazopelastra@gmail.com>
2022-12-07 00:36:21 +0700

    add

---
+++ Exam2/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Введите номер четверти");
+int num=int.Parse(Console.ReadLine()!);
+
+if(num>4){
+System.Console.WriteLine("Такой плоскости не существует!");
+}
+if(num==1){
+System.Console.WriteLine("Координаты X: от 0 до +бесконечности. Координаты Y: от 0 до +бесконечности");
+}
+if(num==2){
+System.Console.WriteLine("Координаты X: от 0 до -бесконечности. Координаты Y: от 0 до +бесконечности");
+}
+if(num==3){
+System.Console.WriteLine("Координаты X: от 0 до -бесконечности. Координаты Y: от 0 до -бесконечности");
+}
+if(num==4){
+System.Console.WriteLine("Координаты X: от 0 до +бесконечности. Координаты Y: от 0 до -бесконечности");
+}
