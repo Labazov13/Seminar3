dcaf6e99d39099aa499aafc8a7095508767ecf02
Nikolay <labazopelastra@gmail.com>
2022-12-07 01:29:43 +0700

    add change

--- Exam3/Program.cs
+++ Exam3/Program.cs
@@ -1,17 +1,19 @@
 ﻿System.Console.WriteLine("Вводим координаты первой точки: ");
-System.Console.Write("Введите координаты точки x,y: ");
+System.Console.Write("Введите координаты точки x: ");
 float x = float.Parse(Console.ReadLine()!);
+System.Console.Write("Введите координаты точки y: ");
 float y = float.Parse(Console.ReadLine()!);
 
 System.Console.WriteLine("Вводим координаты второй точки: ");
-System.Console.Write("Введите координаты точки x,y: ");
+System.Console.Write("Введите координаты точки x: ");
 float x1 = float.Parse(Console.ReadLine()!);
+System.Console.Write("Введите координаты точки y: ");
 float y1 = float.Parse(Console.ReadLine()!);
 float diffx = (x - x1);
 float kvatratx = MathF.Pow(diffx, 2);
 float diffy = (y - y1);
 float kvatraty = MathF.Pow(diffy, 2);
 float sum = kvatratx + kvatraty;
-Console.WriteLine(MathF.Sqrt(sum));
+Console.WriteLine("Расстояние между двумя точками: "+MathF.Round(MathF.Sqrt(sum),2));
 
 
