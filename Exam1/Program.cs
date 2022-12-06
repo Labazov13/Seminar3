a3efe69aeee1531a626197870b957b8fea1b2b8d
Nikolay <labazopelastra@gmail.com>
2022-12-07 00:36:13 +0700

    add

---
+++ Exam1/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.Write("Введите координаты точки x ");
+int x = int.Parse(Console.ReadLine()!);
+System.Console.Write("Введите координаты точки y ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (x > 0 && y > 0)
+{
+Console.WriteLine("I");
+}
+if (x < 0 && y > 0)
+{
+Console.WriteLine("II");
+}
+if (x < 0 && y < 0)
+{
+Console.WriteLine("III");
+}
+if (x > 0 && y < 0)
+{
+Console.WriteLine("IV");
+}
+
