7ff6a260875002b8d5e8dcd418deac4eea6a703e
Nikolay <labazopelastra@gmail.com>
2022-12-07 01:14:22 +0700

    add new program

---
+++ Exam3/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Вводим координаты первой точки: ");
+System.Console.Write("Введите координаты точки x,y: ");
+float x = float.Parse(Console.ReadLine()!);
+float y = float.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Вводим координаты второй точки: ");
+System.Console.Write("Введите координаты точки x,y: ");
+float x1 = float.Parse(Console.ReadLine()!);
+float y1 = float.Parse(Console.ReadLine()!);
+float diffx = (x - x1);
+float kvatratx = MathF.Pow(diffx, 2);
+float diffy = (y - y1);
+float kvatraty = MathF.Pow(diffy, 2);
+float sum = kvatratx + kvatraty;
+Console.WriteLine(MathF.Sqrt(sum));
+
+
